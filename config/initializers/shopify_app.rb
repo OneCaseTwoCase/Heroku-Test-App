4ec9a14ae5cfcb3e98c6a85d327c8c618cd786c0
eekej001 <eekej001@gmail.com>
2016-09-05 05:56:00 -0700

    added aws Hi2.js for testing script tag creation in shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -4,4 +4,9 @@ ShopifyApp.configure do |config|
   config.secret = "78679823ec6c0a7d3256ca55d878f026"
   config.scope = "read_orders, read_products, write_products"
   config.embedded_app = true
+
+  config.scripttags = [
+    {event:'onload', src: 'https://s3-us-west-1.amazonaws.com/hi-js/Hi2.js'}
+  ]
+  
 end
