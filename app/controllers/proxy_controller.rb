f8bcd84499dcb78cf9928f110781bec247cf63d3
eekej001 <eekej001@gmail.com>
2016-09-16 14:27:17 -0700

    added proxy controller

---
+++ app/controllers/proxy_controller.rb
@@ -0,0 +1,25 @@
+class ProxyController < ApplicationController
+
+def index
+ 
+  end
+
+
+def create
+    @form = Form.new(form_params)
+    if @form.save
+      flash[:notice] = "Form saved successfully."
+      redirect_to(:action => 'index') 
+    end  
+    #redirect_to(:action => 'index') 
+  end
+
+
+
+end
+
+
+private
+    def form_params
+      params.require(:form).permit(:first_name, :last_name)
+    end
